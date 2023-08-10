67e3a94b19f2754999f7bede38bb8900a2d2d97a
Céline <32040832+Eniid@users.noreply.github.com>
2023-08-10 16:48:19 +0400

    :lipstick: redesign

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
-var menu = document.getElementById('menu');
-    var closeIcon = document.getElementById("menu_opener");
+const annimElt = document.getElementsByClassName("annim"); 
 
-    menu.addEventListener('click', handleMenuClick);
+console.log(annimElt);
 
-    function handleMenuClick(event) {
-      if (event.target instanceof HTMLAnchorElement) {
-        closeIcon.checked = false;
-      }
+function annime(){
+    console.log("coucou");
+    
+    oneElt.classList.add('annimation_suite')
+}
+
+for(var i= 0; i < annimElt.length; i++)
+{
+    let oneElt = annimElt[i];
+    oneElt.classList.add('annimation')
+    console.log(oneElt);
+}
+
+
+window.addEventListener("scroll", e => { // On fait en sorte que l'action se passe lorsque qu'on scrolle
+document.querySelectorAll(".annim").forEach(element => { // On boucle sur tous les éléments qui ont la class annim
+    const scrollDeLaPage = window.pageYOffset; // On récupère le scroll de la page
+    const positionDeLElement = element.offsetTop; // Puis la position de l'élément .annim
+    const tailleDeLaPage = window.innerHeight // et on prend la taille de la page
+    if (scrollDeLaPage >= positionDeLElement - tailleDeLaPage + 200) { // Si on est plus bas que l'élément en bas + 200px (à modifier si tu veux que l'event se passe plus tôt)
+        element.classList.add("animation_suite"); // on ajoute la class
     }
+})
+}, false)
