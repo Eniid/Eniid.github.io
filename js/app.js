3b6eb0e250a01328cb6aebf66c137cae74abbfe7
Céline <32040832+Eniid@users.noreply.github.com>
2024-09-12 20:06:03 +0400

    :sparkles: new layout

--- js/app.js
+++ js/app.js
@@ -76,3 +76,38 @@ options.on('click', function(e) {
   }, 600);
 });
 
+
+
+
+
+//** Light Box */
+
+
+
+const element = document.querySelector(".revers_scrool")
+element.addEventListener("wheel", e=>{
+    //e.preventDefault()
+    element.scrollLeft += e.deltaY
+}, true)
+
+
+document.addEventListener("click", e=>{
+    const el = e.target;
+    if(el.classList.contains("lightbox_image")){
+        e.preventDefault()
+        const checkbox = document.querySelector("#lightbox");
+        const lightbox = document.querySelector(".lightboxContainer")
+        lightbox.innerHTML = ""
+        const image = document.createElement("img")
+        image.src = el.parentElement.getAttribute("href")
+        lightbox.insertAdjacentElement("beforeend", image)
+        checkbox.checked = true
+        console.log("bouuu");
+        
+        return false; 
+    }
+    if(el.classList.contains("lightbox")){
+        const checkbox = document.querySelector("#lightbox");
+        checkbox.checked = false
+    }
+})
